2883635e026190ff553e2e7d1e8d3ce2af006833
Mitya Selivanov <0x7fffff@guattari.ru>
2022-06-25 20:47:43 +0400

    [test] add allocate 100 test

--- source/test/unittests/memory.test.cpp
+++ source/test/unittests/memory.test.cpp
@@ -10,4 +10,13 @@ namespace coro {
     REQUIRE(p != nullptr);
     deallocate(p);
   }
+
+  TEST_CASE("allocate 100 ints") {
+    auto const size = 100;
+    auto *p = reinterpret_cast<int *>(allocate(sizeof(int) * size));
+    REQUIRE(p != nullptr);
+    for (int i = 0; i < size; i++)
+      p[i] = i;
+    deallocate(p);
+  }
 }
